98f30e9aea58f51f77103691bc3900fc35fb52fd
Hung Nguyen <hungnguyen.cosc@gmail.com>
2024-10-23 15:01:20 +0700

    #feat: add more note to sheet music practice, improve UI

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
-# * Each note is associated with different colors
 NOTE_COLORS = {
     "E": "black",
     "F": "magenta",
@@ -24,19 +23,31 @@ GUITAR_NOTES = {
 }
 
 NOTES_ON_SHEET = [
-    {"note": "E", "on": "line", "position": 1},
-    {"note": "F", "on": "space", "position": 1},
-    {"note": "G", "on": "line", "position": 2},
-    {"note": "A", "on": "space", "position": 2},
-    {"note": "B", "on": "line", "position": 3},
-    {"note": "C", "on": "space", "position": 3},
-    {"note": "D", "on": "line", "position": 4},
-    {"note": "E", "on": "space", "position": 4},
-    {"note": "F", "on": "line", "position": 5}
+    {"note": "A", "on": "line", "position": -2},
+    {"note": "B", "on": "space", "position": -2},
+    {"note": "C", "on": "line", "position": -1},
+    {"note": "D", "on": "space", "position": -1},
+    {"note": "E", "on": "line", "position": 0},
+    {"note": "F", "on": "space", "position": 0},
+    {"note": "G", "on": "line", "position": 1},
+    {"note": "A", "on": "space", "position": 1},
+    {"note": "B", "on": "line", "position": 2},
+    {"note": "C", "on": "space", "position": 2},
+    {"note": "D", "on": "line", "position": 3},
+    {"note": "E", "on": "space", "position": 3},
+    {"note": "F", "on": "line", "position": 4},
+    {"note": "G", "on": "space", "position": 4},
+    {"note": "A", "on": "line", "position": 5},
 ]
 
 # Map positions to the index in the staff
 POSITION_MAP = {
+    ("line", -2): 14,
+    ("space", -2): 13,
+    ("line", -1): 12,
+    ("space", -1): 11,
+    ("line", 0): 10,
+    ("space", 0): 9,
     ("line", 1): 8,
     ("space", 1): 7,
     ("line", 2): 6,
@@ -45,7 +56,7 @@ POSITION_MAP = {
     ("space", 3): 3,
     ("line", 4): 2,
     ("space", 4): 1,
-    ("line", 5): 0
+    ("line", 5): 0,
 }
 
 GUITAR_FRETS = 12
@@ -53,3 +64,16 @@ GUITAR_FRETS = 12
 NUMBER_OF_GUITAR_STRINGS = 6
 
 SECTION_BREAK = "-" * 65
+
+EXTRA_LINE = "┆"
+TAB_LINE = "|"
+TAB_CORNER = {
+    "TOP_LEFT": "┍",
+    "TOP_RIGHT": "┑",
+    "BOTTOM_LEFT": "┕",
+    "BOTTOM_RIGHT": "┙"
+}
+STAFF_LINE = "......."
+STAFF_SPACE = "       "
+SYMBOL_FOR_NOTE = "♪"
+UNKNOWN_NOTE = "?"
--- sheet_music_practice.py
+++ sheet_music_practice.py
@@ -1,24 +1,31 @@
 import random
 from termcolor import colored
 import time
-from constants import NOTES_ON_SHEET, POSITION_MAP, SECTION_BREAK
+from constants import NOTES_ON_SHEET, POSITION_MAP, SECTION_BREAK, TAB_CORNER, TAB_LINE, STAFF_LINE, STAFF_SPACE, EXTRA_LINE, SYMBOL_FOR_NOTE, UNKNOWN_NOTE
 from utils import pretty_time, display_note_reminders
 import sys
 
-# * Define a template for an empty staff
-
 
 def create_empty_staff():
+    """Create an empty staff."""
     return [
-        " _______ ",  # * Line 5 (Top line)
-        "|       |",  # * Space 4
-        "|_______|",  # * Line 4
-        "|       |",  # * Space 3
-        "|_______|",  # * Line 3 (Middle line)
-        "|       |",  # * Space 2
-        "|_______|",  # * Line 2
-        "|       |",  # * Space 1
-        "|_______|"   # * Line 1 (Bottom line)
+        f"{EXTRA_LINE}{STAFF_LINE}{EXTRA_LINE}",
+        f"{EXTRA_LINE}{STAFF_SPACE}{EXTRA_LINE}",
+        # * Top of the musical tablature, above are the extra lines
+        f"{TAB_CORNER['TOP_LEFT']}{STAFF_LINE}{TAB_CORNER['TOP_RIGHT']}",
+        f"{TAB_LINE}{STAFF_SPACE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_LINE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_SPACE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_LINE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_SPACE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_LINE}{TAB_LINE}",
+        f"{TAB_LINE}{STAFF_SPACE}{TAB_LINE}",
+        f"{TAB_CORNER['BOTTOM_LEFT']}{STAFF_LINE}{TAB_CORNER['BOTTOM_RIGHT']}",
+        # * Bottom of musical tablature, below are the extra lines
+        f"{EXTRA_LINE}{STAFF_SPACE}{EXTRA_LINE}",
+        f"{EXTRA_LINE}{STAFF_LINE}{EXTRA_LINE}",
+        f"{EXTRA_LINE}{STAFF_SPACE}{EXTRA_LINE}",
+        f"{EXTRA_LINE}{STAFF_LINE}{EXTRA_LINE}",
     ]
 
 
@@ -26,8 +33,9 @@ def place_notes_on_staff(staff):
     """Place all notes on the staff."""
     for note in NOTES_ON_SHEET:
         pos = POSITION_MAP[(note["on"], note["position"])]
-        staff[pos] = staff[pos][:4] + "♪" + \
-            staff[pos][5:]  # * Replace with whole note '♪'
+        # * Replace the space with the note representation
+        staff[pos] = staff[pos][:4] + SYMBOL_FOR_NOTE + \
+            staff[pos][5:]
 
 
 def hide_random_note(staff):
@@ -36,15 +44,35 @@ def hide_random_note(staff):
     hidden_note = NOTES_ON_SHEET[hidden_note_index]
 
     pos = POSITION_MAP[(hidden_note["on"], hidden_note["position"])]
-    staff[pos] = staff[pos][:4] + colored("?", "black", "on_green") + \
+    staff[pos] = staff[pos][:4] + UNKNOWN_NOTE + \
         staff[pos][5:]  # * Remove the note representation
     return hidden_note
 
 
-def practice_identifying_hidden_note(staff, hidden_note):
-    print("Identify the hidden note on the staff:")
+def pretty_staff(staff):
+    """Pretty print the staff."""
     for line in staff:
-        print(line)
+        if EXTRA_LINE in line:
+            if UNKNOWN_NOTE in line:
+                # * Print the question mark in red, and the rest in blue
+                question_mark_index = line.index(UNKNOWN_NOTE)
+                pre_question_mark = colored(line[:question_mark_index], "blue")
+                post_question_mark = colored(
+                    line[question_mark_index + 1:], "blue")
+                colored_question = colored(UNKNOWN_NOTE, "green")
+                print(pre_question_mark + colored_question + post_question_mark)
+            else:
+                print(colored(line, "blue"))
+        elif UNKNOWN_NOTE in line:
+            print(line.replace(UNKNOWN_NOTE, colored(UNKNOWN_NOTE, "green")))
+        else:
+            print(line)
+
+
+def practice_identifying_hidden_note(staff, hidden_note):
+    """Practice identifying the hidden note."""
+    print("Identify the hidden note on the staff:")
+    pretty_staff(staff)
 
     user_answer = ""
     start_time = time.time()
@@ -56,12 +84,16 @@ def practice_identifying_hidden_note(staff, hidden_note):
         if user_answer == hidden_note["note"]:
             print(
                 f"Correct! The hidden note is {colored(user_answer, 'green')}")
+        else:
+            print(
+                colored("Incorrect! Try again.", "red"))
 
     end_time = time.time()
     return end_time - start_time
 
 
 def sheet_music_practice():
+    """Main function to practice sheet music."""
     print("Welcome to Sheet Music Practice 🎼🎵🎶!")
     display_note_reminders()
 
@@ -70,6 +102,7 @@ def sheet_music_practice():
     while True:
         try:
             staff = create_empty_staff()  # Reset the staff for each round
+
             place_notes_on_staff(staff)    # Place notes on the staff
             hidden_note = hide_random_note(staff)  # Hide a note
 
@@ -82,5 +115,5 @@ def sheet_music_practice():
             if time_records:
                 print(
                     f"\nAverage time taken: {pretty_time(sum(time_records) / len(time_records))}")
-            print("\nThanks for playing! Goodbye! 😊")
+            print("\nHope you had fun practicing sheet music! Goodbye! 😊")
             sys.exit(0)
